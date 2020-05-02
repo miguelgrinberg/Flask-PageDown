0abf4fc53ac8d89e8bacc58fb961db7f323779de
Miguel Grinberg <miguel.grinberg@gmail.com>
2020-05-02 16:27:45 +0100

    Release 0.3.0

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-PageDown',
-    version='0.2.2',
+    version='0.3.0',
     url='http://github.com/miguelgrinberg/flask-pagedown/',
     license='MIT',
     author='Miguel Grinberg',
